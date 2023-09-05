31b4781ca09f2fc60ef142fc373b7fc92c2b2db5
Gavin Lu <gavinlu94@gmail.com>
2023-09-04 21:12:11 -0400

    PP-8: Create Main Pages
        Create components to represent the main pages of the site. Configure
        routing to map URLs to the correct pages.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import EducationPage from './pages/EducationPage';
+import ExperiencePage from './pages/ExperiencePage';
+import ProjectsPage from './pages/ProjectsPage';
+import TimelinePage from './pages/TimelinePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/education' element={<EducationPage />} />
+        <Route path='/experience' element={<ExperiencePage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+        <Route path='/timeline' element={<TimelinePage />} />
+      </Routes>
+    </Router>
   );
 }
 
