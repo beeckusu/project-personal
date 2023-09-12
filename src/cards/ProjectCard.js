b6d9245121a8ea71152cbd822cabd01e947c46b9
Gavin Lu <gavinlu94@gmail.com>
2023-09-11 20:33:49 -0400

    PP-14: Fill in Project Experience

--- src/cards/ProjectCard.js
+++ src/cards/ProjectCard.js
@@ -9,7 +9,10 @@ const ProjectCard = ({ project }) => {
         <Card className="project-card">
             {project.icon && <img src={project.icon} alt="company icon" class="project-icon"></img>}
             <div class="project-preface">
-                <p class='layout-flex title'><b class='title layout-left-aligned'>{project.title}</b><span class='layout-right-aligned date'>{project.startDate} - {project.endDate}</span></p>
+                <p class='layout-flex title'>
+                    <b class='title layout-left-aligned'>{project.title}</b>
+                    <span class='layout-right-aligned date'>{project.endDate ? project.startDate + " - " + project.endDate : project.startDate}</span>
+                </p>
                 <p class='url'>URL: <a href={project.url} target="_blank" rel="noopener noreferrer">{project.url}</a></p>
                 <p class='url'>Git: <a href={project.gitLink} target="_blank" rel="noopener noreferrer">{project.gitLink}</a></p>
                 <p>{project.tags.map((tag) => {
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,18 +1,32 @@
 import { Container } from "react-bootstrap";
 import ProjectCard from "../cards/ProjectCard";
-
+import dcIcon from "../assets/icon-doctorcare.jpg";
+import budgetIcon from "../assets/icon-budget.png";
 
 const projects = [
     {
+        icon: dcIcon,
         title: "Personal Website",
-        startDate: "Sept 2021",
-        endDate: "Present",
+        startDate: "Sep 2023",
         url: "https://www.google.com",
         gitLink: "https://github.com/beeckusu/project-personal",
-        descriptionPoints: ["Designed and developed a personal website using React, Bootstrap, and React-Bootstrap.",
-            "Implemented a custom markdown parser to allow for easy content editing.",
+        descriptionPoints: [
+            "Designed and developed a personal website using React, Bootstrap, and React-Bootstrap.",
             "Deployed the website using GitHub Pages."],
-        tags: ["React", "Javascript", "HTML", "CSS", "Git", "Bootstrap", "React-Bootstrap", "GitHub Pages"]
+        tags: ["React", "Javascript", "HTML", "CSS", "Git", "Bootstrap", "React-Bootstrap"]
+    },
+    {
+        icon: budgetIcon,
+        title: "Where my Money at Lebowski?",
+        startDate: "Aug 2023",
+        url: "https://beeckusu.github.io/project-budget/",
+        gitLink: "https://github.com/beeckusu/project-budget",
+        descriptionPoints: [
+            "Developed a web application to ingest transaction histories to track spending habits and trends.",
+            "Designed a minimalist user interface for users to customize how the data is displayed.",
+            "Implemented SMACSS organization structure to maintain CSS rules.",
+        ],
+        tags: ["React", "Javascript", "HTML", "CSS", "Git", "Bootstrap", "React-Bootstrap"]
     },
 ]
 
